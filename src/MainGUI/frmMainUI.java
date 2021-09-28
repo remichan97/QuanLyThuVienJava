dcfc29e620d2b998590223a452356aa141263e1a
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-09-28 19:29:48 +0700

    Fix: Canh form đăng nhập, giao diện chính của phần mềm ra giữa màn hình, bắt đầu thiết kế GUI form chính

--- src/MainGUI/frmMainUI.java
+++ src/MainGUI/frmMainUI.java
@@ -16,6 +16,7 @@ public class frmMainUI extends javax.swing.JFrame {
      */
     public frmMainUI() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -28,16 +29,17 @@ public class frmMainUI extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Quản lý thư viện");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 734, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 476, Short.MAX_VALUE)
         );
 
         pack();
--- src/frmSignIn.java
+++ src/frmSignIn.java
@@ -17,6 +17,7 @@ public class frmSignIn extends javax.swing.JFrame {
      */
     public frmSignIn() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -128,9 +129,9 @@ public class frmSignIn extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+        /* Set the Windows look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+        /* If Windows (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
